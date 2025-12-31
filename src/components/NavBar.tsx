e54e3dd29af547360868fc4137a4415fed57e596
“Brian <briancoder402@gmail.com>
2025-12-31 09:27:06 -0300

    feature/navbar-init-base. Dai rompe bola

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,31 +1,54 @@
+import { Search } from "lucide-react"
 import { NavLink } from "react-router-dom"
 
 const NavBar = () => {
   return (
     <header>
         <span className=" block "> Ofertas! </span>
-        <section>
-            <NavLink to='\hombres'>
-                <p>Hombre</p>
-            </NavLink>
-            <NavLink to='\mujeres'>
-                <p>Mujer</p>
-            </NavLink>
-            <NavLink to='\perdumeria'>
-                <p>Perfumeria</p>
-            </NavLink>
-            <NavLink to='\hombres'>
-                <p>Accesorios</p>
-            </NavLink>
+        <section className="flex justify-space">
+            <nav>
+                <ul className="flex justify-center items-center gap-4 text-[0.875rem]">
+                        <li>
+                            <NavLink to='/category/Hombres'>HOMBRES</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/category/Mujeres'>MUJERES</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/category/Nosotros'>REGALOS</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/'>ACCESORIOS</NavLink>
+                        </li>
+                </ul>
+            </nav>
+            <div>
+                <NavLink to="/" className="no-underline">
+                    {/* AQUÍ ESTÁ LA MAGIA: Agregamos 'font-logo' */}
+                    <span className="font-logo text-3xl font-bold tracking-wide text-black transition-colors duration-500">
+                        Messina
+                    </span>
+                </NavLink>
+            </div>
+            <ul className="iconosNavBar flex justify-center gap-6 ">
+                    <li>
+                        <NavLink to='/cart'>
+                            <Search />
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/cart'>
+                            
+                        </NavLink>
+                    </li>
+                    <li>
+                        <a href="#">
+                            
+                        </a>
+                    </li>
+                </ul>
         </section>
-        <section>
-            <NavLink to='\'>
-                    <img src="../assets/MessinaIcon.png" alt="Logo Messina" />
-            </NavLink>
-        </section>
-        <section>
 
-        </section>
     </header>
   )
 }
